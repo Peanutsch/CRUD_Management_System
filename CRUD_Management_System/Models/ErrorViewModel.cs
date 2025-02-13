cc0afca8db7efc69d52c6214a16e3d7bd9d6931c
Gebruiker <Gebruiker@LOK01-10>
2025-02-13 10:30:49 +0100

    Add project files.

---
+++ CRUD_Management_System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
