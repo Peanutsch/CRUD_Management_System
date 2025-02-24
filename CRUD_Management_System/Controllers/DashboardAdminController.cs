9e2f206b6ecf2391548ef4647b21f89f5d25e7f7
peanutsch <peanutsch@duck.com>
2025-02-24 13:14:20 +0100

    Table Users /
    * Set userlist at max 6 per page
    * Alias of Current User passed to View/DashboardAdmin/Index correctly

--- CRUD_Management_System/Controllers/DashboardAdminController.cs
+++ CRUD_Management_System/Controllers/DashboardAdminController.cs
@@ -2,6 +2,8 @@
 using CRUD_Management_System.Models;
 using Microsoft.EntityFrameworkCore;
 using CRUD_Management_System.Data;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace CRUD_Management_System.Controllers
 {
@@ -14,11 +16,16 @@ namespace CRUD_Management_System.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult>Index()
+        public async Task<IActionResult>Index(int? page)
         {
+            int pageSize = 6; // Maximaal aantal gebruikers per pagina
+            int pageNumber = page ?? 1; // Standaard op pagina 1 starten
+
             var users = await _context.UserDetails.ToListAsync();
+            var pagedUsers = users.ToPagedList(pageNumber, pageSize);
+
             ViewData["CurrentUser"] = TempData["CurrentUser"]; // Haal de waarde uit TempData
-            return View(users);
+            return View(pagedUsers);
         }
     }
 }
