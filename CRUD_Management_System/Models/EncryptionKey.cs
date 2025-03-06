ce220e245dbf1648f702589cf76f606671295520
peanutsch <peanutsch@duck.com>
2025-03-06 12:45:08 +0100

    Encryption
    * Added encrypted login- and user details
    * Added encryptionkey in csv

---
+++ CRUD_Management_System/Models/EncryptionKey.cs
@@ -0,0 +1,8 @@
+﻿namespace CRUD_Management_System.Models
+{
+    public class EncryptionKey
+    {
+        public int Id { get; set; }
+        public required string KeyContent { get; set; }
+    }
+}
